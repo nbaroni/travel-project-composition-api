aab4af7dc018c1de3a7971a69f6a5cce22661283
nbaroni <nbaroni@bayviewglen.ca>
2023-02-28 22:29:51 -0500

    ported over from the main repo

---
+++ src/views/Protected.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Greetings, {{ username }}</h1>
+        <router-link :to="{name: 'home'}">
+            <button @click="logout">Logout</button>
+        </router-link>
+        
+        <router-link :to="{name: 'invoices'}">
+            <button>Invoices</button>
+
+        </router-link>
+    </div>
+</template>
+
+<script>
+import {useRouter, useRoute} from 'vue-router'
+import {ref} from 'vue'
+
+export default {
+    setup () {
+        const username = ref(window.user)
+
+        const logout = () => {
+            window.user = null;
+        }
+
+        return {username, logout}
+    }
+}
+
+
+</script>
